6978f55baeefb4eb6f204eb6c12ac9183d50f614
Rafael Cruz <jose_rafael_cruz@claro.com.do>
2023-06-01 17:02:48 -0400

    Feat: add output error

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,16 @@
-﻿Console.WriteLine("Start App");
-CancellationTokenSource cancellationToken = new();
-Console.CancelKeyPress += (_, e) =>
+﻿try
 {
-    e.Cancel = true; // prevent the process from terminating.
-    cancellationToken.Cancel();
-};
+    Console.WriteLine("Start App");
+    CancellationTokenSource cancellationToken = new();
+    Console.CancelKeyPress += (_, e) =>
+    {
+        e.Cancel = true; // prevent the process from terminating.
+        cancellationToken.Cancel();
+    };
+}
+catch (IOException e)
+{
+    TextWriter errorWriter = Console.Error;
+    errorWriter.WriteLine($"Error: {e.Message}");
+    return;
+}
