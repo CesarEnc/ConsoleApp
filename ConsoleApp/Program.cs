2d68a763b0e27d9ff48f9ed8fb9fa6f49ee7cbee
Rafael Cruz <jose_rafael_cruz@claro.com.do>
2023-06-01 16:42:04 -0400

    Feat: Add cancelation Token

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,7 @@
 ﻿Console.WriteLine("Start App");
+CancellationTokenSource cancellationToken = new();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true; // prevent the process from terminating.
+    cancellationToken.Cancel();
+};
